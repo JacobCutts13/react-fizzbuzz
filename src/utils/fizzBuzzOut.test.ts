0cab953fa84e1f78a2cb00c7776a3907848c2701
Jc <Jc@email.com>
2022-03-08 17:41:07 +0000

    format

--- src/utils/fizzBuzzOut.test.ts
+++ src/utils/fizzBuzzOut.test.ts
@@ -1,6 +1,6 @@
 import fizzBuzzOut from "./fizzBuzzOut";
 
 test("fizzBuzzOut takes in a number and returns how the number is said in the game", () => {
-    expect(fizzBuzzOut(5)).toBe("Buzz");
-    expect(fizzBuzzOut(76)).toBe("76");
-  });
+  expect(fizzBuzzOut(5)).toBe("Buzz");
+  expect(fizzBuzzOut(76)).toBe("76");
+});
